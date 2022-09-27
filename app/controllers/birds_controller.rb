87818c015f3dd3087604bc23bb921fff15f45d76
jbdura <bidurajohnson@gmail.com>
2022-09-27 15:50:19 +0300

    Done

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -3,7 +3,7 @@ class BirdsController < ApplicationController
   # GET /birds
   def index
     birds = Bird.all
-    render json: birds
+    render json: birds, except: [:created_at, :updated_at]
   end
 
   # GET /birds/:id
@@ -15,5 +15,14 @@ class BirdsController < ApplicationController
       render json: { error: "Bird not found" }, status: :not_found
     end
   end
+
+  # POST /birds
+  def create
+    # byebug
+    bird = Bird.create(name: params[:name], species: params[:species])
+    render json: bird, status: :created
+  end
+
+  #etc
 
 end
